63be3a6cdc9f4292d20cc62d720aca0bcd43887a
Tellmore01 <88238579+Tellmore01@users.noreply.github.com>
2023-02-10 14:30:12 -0600

    Testing divs

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="center">
       <h1 className="typing-text">Mostafa's Website..</h1>
+      <div className="container1">Project 1</div>
+      <div className="container2">Project 2</div>
+      <div className="container3">Project 3</div>
     </div>
   );
 }
