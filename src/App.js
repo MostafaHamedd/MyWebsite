b1ae4802ccf8dd952a9627d4e2e893c20a7d0a28
Tellmore01 <88238579+Tellmore01@users.noreply.github.com>
2023-02-11 15:13:10 -0600

    Added parameters to the project function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import "./App.css";
 import image from "./automation.jpg";
+import courseTracker from "./CourseTracker.png";
+import ims from "./IMS.png";
 import React, { Component } from "react";
 
-function project() {
+function project(img, url, title) {
   return (
-    <a href="https://www.google.com">
-      <img className="photo" src={image} alt="pic 1" />;
+    <a href={url}>
+      <img title={title} className="photo" src={img} alt="pic 1" />
     </a>
   );
 }
 function App() {
   return (
     <div>
-      <h1 className="typing-text">Mostafa's Website..</h1>
+      <h1 className="typing-text">Mostafa Hamed</h1>
       <div className="containerTest">
-        <div className="container1"> {project()}</div>
-        <div className="container1"> {project()}</div>
-        <div className="container1"> {project()}</div>
-        <div className="container1"> {project()}</div>
+        <div className="container1">
+          {project(
+            courseTracker,
+            "https://github.com/MostafaHamedd/Course-grade-tracker",
+            "Course Tracker"
+          )}
+        </div>
+        <div className="container1">
+          {project(
+            ims,
+            "https://github.com/MostafaHamedd/Inventory-Management-System",
+            "IMS"
+          )}
+        </div>
+        <div className="container1">
+          {project(
+            image,
+            "https://github.com/MostafaHamedd/UManitoba-Course-Registration-Automation-Educational-Purposes-",
+            "Auotomated Registration"
+          )}
+        </div>
+        <div className="container1">
+          {project(
+            ims,
+            "https://github.com/MostafaHamedd/Tellmore01.github.io",
+            "Github.io"
+          )}
+        </div>
       </div>
     </div>
   );
